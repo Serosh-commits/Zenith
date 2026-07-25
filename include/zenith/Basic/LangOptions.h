ab9fcb2188b05737b51d258c46ae180001deb157
Serosh-commits <janmejayapanda400@gmail.com>
2026-07-25 09:24:49 +0530

    Add language options header

---
+++ include/zenith/Basic/LangOptions.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace zenith {
+
+struct LangOptions {
+    bool CPlusPlus = false;
+    bool CPlusPlus11 = false;
+    bool CPlusPlus17 = false;
+    bool CPlusPlus20 = false;
+    bool C99 = true;
+    bool C11 = false;
+    bool Trigraphs = false;
+    bool LineComment = true;
+    bool Bool = false;
+    bool HexFloats = true;
+    bool Digraphs = false;
+
+    LangOptions() = default;
+};
+
+}
